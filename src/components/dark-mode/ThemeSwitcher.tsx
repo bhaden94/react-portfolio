39f0cebbc216acd1c1e8b84809295688be5721cb
bhaden94 <bhaden94@yahoo.com>
2020-12-20 18:37:14 -0800

    theme switcher iconbutton taken out

--- src/components/dark-mode/ThemeSwitcher.tsx
+++ src/components/dark-mode/ThemeSwitcher.tsx
@@ -1,11 +1,13 @@
 import { makeStyles, Theme, createStyles, useTheme } from '@material-ui/core/styles';
 import { useChangeTheme } from '../../theme';
-import IconButton from '@material-ui/core/IconButton';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        container: {
+            padding: '5px 0 15px 0'
+        },
         icon: {
             color: theme.palette.secondary.main,
             '&:hover': {
@@ -23,11 +25,11 @@ export default function ThemeSwitcher() {
     const classes = useStyles();
 
     return (
-        <IconButton aria-label="dark mode" onClick={changeTheme}>
+        <div className={classes.container}>
             {theme.palette.type === 'light' ?
-                <Brightness7Icon fontSize='large' className={classes.icon} /> :
-                <Brightness4Icon fontSize='large' className={classes.icon} />
+                <Brightness7Icon onClick={changeTheme} fontSize='large' className={classes.icon} /> :
+                <Brightness4Icon onClick={changeTheme} fontSize='large' className={classes.icon} />
             }
-        </IconButton>
+        </div>
     );
 }
