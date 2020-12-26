f54970c99793887958a6001a422a39ec1cd2db61
bhaden94 <bhaden94@yahoo.com>
2020-12-26 10:44:23 -0800

    remove unused imports

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import Button from '@material-ui/core/Button';
 import { ContactObj, ContactObject } from '../../infoObjects/ContactObj'
 
