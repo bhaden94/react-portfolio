69f0db6428f6579251efe699273a2e0dbbcb68fc
bhaden94 <bhaden94@yahoo.com>
2021-01-03 09:21:14 -0800

    return to top button works on mobile

--- src/components/contact/ReturnToTop.tsx
+++ src/components/contact/ReturnToTop.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-// import Link from "@material-ui/core/Link";
 import { Link } from "react-scroll";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -9,11 +8,25 @@ const useStyles = makeStyles((theme: Theme) =>
 		returnTopContainer: {
 			position: "absolute",
 			bottom: 0,
+			[theme.breakpoints.down("sm")]: {
+				bottom: 60,
+			},
+			"&:hover $returnTop": {
+				color:
+					theme.palette.type === "dark"
+						? theme.palette.primary.main
+						: theme.palette.secondary.main,
+			},
 			width: "100%",
 			textAlign: "center",
 		},
 		returnTop: {
 			textAlign: "center",
+			transition: "color 250ms",
+			color:
+				theme.palette.type === "dark"
+					? theme.palette.secondary.main
+					: theme.palette.primary.main,
 		},
 	})
 );
@@ -22,12 +35,11 @@ function ReturnToTop() {
 	const classes = useStyles();
 
 	return (
-		<Link to={"top"} smooth={true} duration={300}>
+		<Link to={"top"} smooth={true} duration={500}>
 			<div className={classes.returnTopContainer}>
 				<ArrowUpwardIcon
 					className={classes.returnTop}
 					fontSize="large"
-					color="secondary"
 				/>
 			</div>
 		</Link>
