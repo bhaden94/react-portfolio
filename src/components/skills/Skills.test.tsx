dfa91df86a66a87906dcee00fc6db9f3ee49fcbc
bhaden94 <bhaden94@yahoo.com>
2021-04-25 17:08:41 -0400

    skills component tests written

--- src/components/skills/Skills.test.tsx
+++ src/components/skills/Skills.test.tsx
@@ -2,9 +2,22 @@ import { render, screen } from "@testing-library/react";
 import Skills from "./Skills";
 import { SkillsObject, SkillsObj } from "../../infoObjects/SkillsObj";
 
-
-it("renders Skills component with objects", () => {
-    const skills: SkillsObj[] = SkillsObject();
+it("renders Skills component with object titles", () => {
+	const skills: SkillsObj[] = SkillsObject();
 	render(<Skills />);
-	expect(screen.getByText(skills[0].title)).toBeInTheDocument();
+
+	skills.forEach((skill: SkillsObj) =>
+		expect(screen.getByText(skill.title)).toBeInTheDocument()
+	);
+});
+
+it("renders Skills component with object tech names", () => {
+	const skills: SkillsObj[] = SkillsObject();
+	render(<Skills />);
+
+	skills.forEach((skill: SkillsObj) =>
+		skill.tech.forEach((tech: any) =>
+			expect(screen.getAllByText(tech.name)[0]).toBeInTheDocument()
+		)
+	);
 });
