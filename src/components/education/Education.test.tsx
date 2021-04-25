c81f65ef0398b1136d54f9001f39f70de7fd55c1
bhaden94 <bhaden94@yahoo.com>
2021-04-25 17:30:14 -0400

    fix name for LandingPage test file an add tests for Education component

---
+++ src/components/education/Education.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { EducationObject, EducationObj } from "../../infoObjects/EducationObj";
+import Education from "./Education";
+
+beforeEach(() => {
+	render(<Education />);
+});
+
+it("renders Education component with school names", () => {
+	const education: EducationObj[] = EducationObject();
+	education.forEach((ed: EducationObj) =>
+		expect(screen.getAllByText(ed.school)[0]).toBeInTheDocument()
+	);
+});
+
+it("renders Education component with degree names", () => {
+	const education: EducationObj[] = EducationObject();
+	education.forEach((ed: EducationObj) =>
+		expect(screen.getByText(ed.degree)).toBeInTheDocument()
+	);
+});
