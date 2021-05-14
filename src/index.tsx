55bce5c68c4391120e2cc2a1cae41f8efb7f30a1
bhaden94 <bhaden94@yahoo.com>
2021-05-13 20:35:17 -0400

    import animate css and update class name for animation library use. Also update about section and site description with title.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "animate.css/animate.min.css";
 
 ReactDOM.render(
 	<React.StrictMode>
